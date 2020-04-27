040e15f712482b89e3366371cbefa82451c13265
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-27 20:05:46 +0000

    Automatically backed up by Learn

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -1,11 +1,12 @@
+
 require_relative '../lib/position_taken.rb'
+
 
 describe '#position_taken? in lib/position_taken.rb' do
   it 'accepts a board and the index to check as arguments' do
     board = []
     index = 0
-
-    expect{position_taken?(board, index)}.to_not raise_error
+    expect(position_taken?(board, index)).to be(false)
   end
 
   it 'returns false if the board has an empty string " " in the desired index' do
