5704ec17237aa3e03628f73b0f7154dfa1290ade
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-27 20:09:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,9 @@
 def position_taken?(board, index)
   if (board[index] == "X" || board[index] == "O")
     return(true)
-  elsif board[index] == (" ", "", nil)
+  elsif board[index] == " "
+    return(false)
+  elsif board[index] == ""
     return(false)
   else
     return(false)
